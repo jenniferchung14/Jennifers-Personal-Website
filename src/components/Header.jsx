1a865f1074a86507112ac56780a0ba1fa0c171b9
Jennifer Chung <jennifer.chung14@gmail.com>
2025-04-21 18:00:32 -0400

    fixed the auto close for the hambuger on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import jennifer from '../assets/favicon.png';
 import resume from '../assets/Resume - Jennifer Chung.pdf';
 
 function Header() {
-  return (
-    <>
-        <nav class="navbar">
-            <div class="logo">
-                <a href="/main"><img src={jennifer} alt="Jennifer Chung" class="logo"/></a>
-            </div>
+    
+    const checkboxRef = useRef(null);
 
-            <ul class="nav-links">
-                <input type="checkbox" id="checkbox_toggle" />
-                <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-
-                <div class="menu">
-                    <li><Link to="/jennifer">A Deeper Dive!</Link></li>
-                    <li><a href={resume} target="_blank">Jenn's Journey!</a></li>
-                    <li><Link to="/projects">Code Gallery!</Link></li>
+    const closeMenu = () => {
+      if (checkboxRef.current) {
+        checkboxRef.current.checked = false;
+      }
+    };
+    
+    return (
+        <>
+            <nav className="navbar">
+                <div className="logo">
+                    <a href="/main"><img src={jennifer} alt="Jennifer Chung" className="logo"/></a>
                 </div>
-            </ul>
-        </nav>
-    </>
-  );
+
+                <ul className="nav-links">
+                    <input type="checkbox" id="checkbox_toggle" ref={checkboxRef}/>
+                    <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
+
+                    <div className="menu">
+                        <li><Link to="/jennifer" onClick={closeMenu}>A Deeper Dive!</Link></li>
+                        <li><a href={resume} target="_blank" rel="noopener noreferrer" onClick={closeMenu}>Jenn's Journey!</a></li>
+                        <li><Link to="/projects" onClick={closeMenu}>Code Gallery!</Link></li>
+                    </div>
+                </ul>
+            </nav>
+        </>
+    );
 }
 
 export default Header;

0c71294043cac8faf6c18c55686e72971e9182fc
Jennifer Chung <jennifer.chung14@gmail.com>
2025-07-08 16:09:30 -0400

    updating with current info

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,8 +8,8 @@ function Main() {
       <div class="container">
         <div class="text-area">
             <h1>Nice to meet you!</h1>
-            <p>I'm Jennifer, a fourth year Computer Science student at <b><a href="https://www.torontomu.ca/" class="a-links" target="_blank">Toronto Metropolitan University</a></b> <i>{"(formerly Ryerson)"}</i>, where I am also minoring in Psychology. I'm an aspiring Software Engineer who currently has a keen interest in web and front-end development.</p>
-            <p>I'm currently looking for a Summer 2025 internship position!</p>
+            <p>I'm Jennifer, I am in my fifth and final year Computer Science student at <b><a href="https://www.torontomu.ca/" class="a-links" target="_blank">Toronto Metropolitan University</a></b> <i>{"(formerly Ryerson)"}</i>, where I am also minoring in Psychology. I'm an aspiring Software Engineer who currently has a keen interest in web and front-end development.</p>
+            <p>I'm currently looking for a full-time new grad position!</p>
             <p>Click around and continue your joruney of getting to know more about me or click <b><a href="/" class="a-links">reset</a></b> and begin your journey again!! {":)"}</p>
         </div>
 
